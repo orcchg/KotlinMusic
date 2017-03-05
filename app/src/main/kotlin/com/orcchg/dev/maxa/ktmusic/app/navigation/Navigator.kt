610547ec2a6146cd81ccda9111e3032bb26b4ac4
Maxim Alov <alovmax@yandex.ru>
2017-03-05 17:20:39 +0300

    Enabled navigation

--- app/src/main/kotlin/com/orcchg/dev/maxa/ktmusic/app/navigation/Navigator.kt
+++ app/src/main/kotlin/com/orcchg/dev/maxa/ktmusic/app/navigation/Navigator.kt
@@ -2,18 +2,20 @@ package com.orcchg.dev.maxa.ktmusic.app.navigation
 
 import android.content.Context
 import com.orcchg.dev.maxa.ktmusic.app.injection.PerActivity
+import com.orcchg.dev.maxa.ktmusic.app.ui.music.detail.ArtistDetailsActivity
+import com.orcchg.dev.maxa.ktmusic.app.ui.music.list.ArtistListActivity
 import javax.inject.Inject
 
 @PerActivity
 class Navigator @Inject constructor() {
 
     fun openListScreen(context: Context) {
-//        val intent = ArtistListActivity.getCallingIntent(context)
-//        context.startActivity(intent)
+        val intent = ArtistListActivity.getCallingIntent(context)
+        context.startActivity(intent)
     }
 
     fun openDetailsScreen(context: Context, artistId: Long) {
-//        val intent = ArtistDetailsActivity.getCallingIntent(context, artistId)
-//        context.startActivity(intent)
+        val intent = ArtistDetailsActivity.getCallingIntent(context, artistId)
+        context.startActivity(intent)
     }
 }
